0073fec82aa2e941d9c1210b6ecfdb865e545ae5
Dagar165 <134097703+Dagar165@users.noreply.github.com>
2026-03-11 12:21:27 +0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router as WouterRouter, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -15,6 +15,7 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
+  <WouterRouter base="/Blender-Buddy-App">
     <div className="relative w-full h-full pb-[80px]">
       <Switch>
         <Route path="/" component={PetPage} />
@@ -24,8 +25,8 @@ function Router() {
         <Route component={NotFound} />
       </Switch>
     </div>
-  );
-}
+  </WouterRouter>
+);
 
 function App() {
   // Initialize Telegram WebApp if available
